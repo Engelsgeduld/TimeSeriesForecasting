365dc6029af29bd331308b8a7a29a03a9f0d1c66
Engelsgeduld <engelsgeduld@MacBook-Pro-Sladkaa.local>
2025-06-16 12:41:38 +0300

    Fix: data leak in DateRangeFilled and GroupByDate, unify DateRangeFilled for datasets

--- src/special_preprocessing/second_special_pipeline/pipeline.py
+++ src/special_preprocessing/second_special_pipeline/pipeline.py
@@ -1,21 +1,23 @@
 from sklearn.pipeline import Pipeline
 
 from src.special_preprocessing.date_transformers.features_extraction import FeatureExtractionTransformer
+from src.special_preprocessing.date_transformers.series_comp import DateRangeFilledTransformer
 from src.special_preprocessing.date_transformers.series_decomposition import Separation, SeriesDecompositionTransformer
 from src.special_preprocessing.second_special_pipeline.preprocessing import (
     CategoricalFeaturesTransform,
-    DateRangeFilledTransformerSec,
     DiscountTransformer,
     KeyTransformer,
     RenameColumns,
 )
+
+fill_strategy = {"ship": 0, "discount": 0, "discount.1": 0}
 
 preprocessing_pipeline = Pipeline(
     steps=[
         ("rename", RenameColumns()),
         ("key", KeyTransformer()),
         ("discount", DiscountTransformer()),
-        ("fill_data_range", DateRangeFilledTransformerSec()),
+        ("fill_data_range", DateRangeFilledTransformer(fill_config=fill_strategy)),
         ("categorical_features_prep", CategoricalFeaturesTransform()),
         ("features extraction", FeatureExtractionTransformer()),
         ("decomposition", SeriesDecompositionTransformer()),
